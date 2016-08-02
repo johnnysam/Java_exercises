bd9a6b9c288f3b72365c68799bd91d4468d0d9a7
Ioannis Samartzis <ioannis.samartzis@gmail.com>
2016-08-02 21:24:01 +0200

    minor changes

--- CryptoTest.java
+++ CryptoTest.java
@@ -6,18 +6,22 @@ public class CryptoTest
 	public static void main (String[] args)
 	{
 		Crypto mCrypto = new Crypto();
-		System.out.print("Please enter number to encode: ");
-
 		Scanner scan = new Scanner(System.in);
+		System.out.print("Please enter number to encode or -1 to quit: ");
 		int input = scan.nextInt();
 
-		int output = mCrypto.encode(input);
+		while (input != -1)
+		{
+			int output = mCrypto.encode(input);
 
-		System.out.printf("Encoded %d to %d\n", input, output);
+			System.out.printf("Encoded %d to %d\n", input, output);
 
-		int decode = mCrypto.decode(output);
+			int decode = mCrypto.decode(output);
 
-		System.out.printf("Decoded %d to %d\n", output, decode);
+			System.out.printf("Decoded %d to %d\n", output, decode);
 
+			System.out.print("Please enter number to encode or -1 to quit: ");
+			input = scan.nextInt();
+		}
 	}
 }
